c69f72c9afa6ff7d095c0b709bf5a9db1754d847
Maykon Granemann <maykondgranemann@gmail.com>
2025-09-22 20:29:46 -0300

    fix(structure): adicionar a pasta da solução

--- BemEstar.Dica.Models/DicaModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BemEstar.Dica.Models;
-
-public class DicaModel : BaseModel
-{
-    public string Titulo { get; set; }
-    public string Descrição { get; set; }
-    public string Categoria { get; set; }
-}
---
+++ BemEstar/BemEstar.Dica.Models/DicaModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BemEstar.Dica.Models;
+
+public class DicaModel : BaseModel
+{
+    public string Titulo { get; set; }
+    public string Descrição { get; set; }
+    public string Categoria { get; set; }
+}
