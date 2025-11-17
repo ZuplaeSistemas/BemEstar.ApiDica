5ec1e120104ed7fc438654003a60ac9413480505
Vanessa <venezareal@gmail.com>
2025-11-17 16:35:41 -0300

    Feat: Estrutura de multiconexões DB

--- BemEstar/BemEstar.Infra/Config/AppConfiguration.cs
+++ BemEstar/BemEstar.Infra/Config/AppConfiguration.cs
@@ -14,9 +14,9 @@ namespace BemEstar.Dica.Infra.Config
         {
             this._configuration = configuration;
         }
-        public string GetConnectionString() 
+        public string GetConnectionString(string dbName) 
         {
-            return _configuration.GetConnectionString("Postgres");
+            return _configuration.GetConnectionString(dbName);
         }
     }
 }
